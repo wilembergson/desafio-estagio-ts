5a0360903cb18123d0390993d1f8863854adf8ea
wilembergson <wilembergson@yahoo.com>
2021-06-30 19:35:59 -0300

    Cards

--- src/components/Tarefa/Tarefa.jsx
+++ src/components/Tarefa/Tarefa.jsx
@@ -1,11 +1,19 @@
-
+import './Tarefa.css'
+import {BiEdit} from 'react-icons/bi'
+import {IoIosCloseCircle} from 'react-icons/io'
 
 export default function Tarefa({tarefas}){
     return(
         <div className="lado-direito">
             <h2>Tarefas a serem concluídas</h2>
             {tarefas.map(t => (
-                <div>{t.texto}</div>
+                <div className="tarefaObj">
+                    <label>{t.texto}</label>
+                    <div className="icones">
+                        <button className="botao"><BiEdit/></button>
+                        <button className="botao"><IoIosCloseCircle/></button>
+                    </div>
+                </div>
             ))}
         </div>
     )
